331f49089bcc2cb5bb789a18a9210930f470e1f0
faribasra6 <faribasra6@gmail.com>
2025-02-13 12:24:59 +0400

    Hero section

---
+++ src/components/search.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+const Search = ({searchState, setSearchState}  ) => {
+    
+    return (
+        <div className="search">
+        <div>
+          <img src="search.svg" alt="search icon" />
+          <input
+            type="text"
+            placeholder="Search through movies"
+            value={searchState || ""}
+            onChange={(e) => setSearchState(e.target.value)}
+          />
+        </div>
+      </div>
+          
+    );
+};
+
+export default Search;
